651367eafec0893fbd8c861e4729032cc519bfd5
DESKTOP-H2P43FG\gcv11 <gianni111@gmail.com>
2018-06-18 14:06:33 +0200

    This is the home page

--- public/index.php
+++ public/index.php
@@ -1,14 +1,73 @@
-<!doctype html>
+<html>
+<head>
 
-<html lang="en">
-	<head>
-		<title>Roomy</title>
-		<meta charset="utf-8">
-	</head>
+<title> ROOMY </title>
+
+<link href ="style.css" rel = "stylesheet" type "text/css">
+<style>
+
+div.hero{
+	text-align:center;
+}
+</style>
+</head>
+
+
+<body >
+	<header>
+
+	<div class = "row">
+		<div class = "logo">
+			ROOMY
+		</div>
+		
+			<form class = "search-form">
+		<input type = "text" placeholder= "search">
+		<button>Search</button>
+		</form>
+		
+
+		
+		
+	<ul class = "main-nav">
 	
-	<body>
 	
-		<h1>Find your Roomy</h1>
+	<li class="active"> <a href=""> HOME </a> </li>
+	<li> <a href=""> SERVICES </a> </li>
+	<li> <a href=""> ABOUT </a> </li>
+	<li> <a href=""> CONTACT </a> </li>
+	<li> <a href=""> FAQ </a> </li>
 	
-	</body>
+	</ul>
+	
+	
+	</div>
+	
+	<div class = "place">
+	
+	<h1> Search for a place!</h1>
+	
+	</div>
+	
+	
+
+	
+</header>	
+	
+<div class ="buttones">
+	
+		<a href = "" class="btn btn-one"> search</a>
+		<a href="" class= "btn btn-two"> register </a>
+		<a href="" class= "btn btn-three"> login </a>
+		
+	</div>
+	
+	
+		
+
+	
+	
+
+</body>
+
 </html>
